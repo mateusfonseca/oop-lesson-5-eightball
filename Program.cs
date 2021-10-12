8a4e3315858c654f9325cdd790449a1706cc2d4c
mateusfonseca <mateusff8@gmail.com>
2021-10-12 15:05:48 +0100

    1st Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace oop_lesson_5_eightball
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
