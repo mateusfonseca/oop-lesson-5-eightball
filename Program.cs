3e4e3571df6c5f25c61d6082453af15595b7384c
mateusfonseca <mateusff8@gmail.com>
2021-10-17 17:15:02 +0100

    2nd Commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+﻿using System.Collections.Generic;
 
 namespace oop_lesson_5_eightball
 {
@@ -6,7 +7,35 @@ namespace oop_lesson_5_eightball
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<string> answers = new List<string> {
+              "It is certain.",
+              "It is decidely so.",
+              "Without a doubt.",
+              "Yes definitely.",
+              "You may rely on it.",
+              "As I see it, yes.",
+              "Most likely.",
+              "Outlook good.",
+              "Yes.",
+              "Signs point to yes.",
+              "Reply hazy, try again.",
+              "Ask again later.",
+              "Better not tell you now.",
+              "Cannot predict now.",
+              "Concentrate and ask again.",
+              "Don't count on it.",
+              "My reply is no.",
+              "My sources say no.",
+              "Outlook not so good.",
+              "Very doubtful."
+            };
+
+            Random rnd = new Random();
+
+            Console.Write("Question: ");
+            Console.ReadLine();
+
+            Console.WriteLine(answers[rnd.Next(21)]);
         }
     }
 }
